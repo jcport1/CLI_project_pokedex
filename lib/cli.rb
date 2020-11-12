408b1ac4ac97d2928a947d534f3081aa2c394670
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-11-12 11:08:02 -0500

    project

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,13 +2,25 @@ class Cli
 
     def start
         puts "*--------------------*"
-        puts "Welcome to Pallet town , Ash Ketchum!"
+        puts "Welcome to Pallet Town, Ash Ketchum!"
         puts "*---------------------*"
         puts "My name is Professor Oak"
-        puts "Wannna explore the pokedex? Y or N"
-        @input = gets.strip.downcase
-        # API.grab_pokemons
+        puts "*---------------------*"
+        self.menu 
+    
     end 
 
+    def menu
+        puts "Wannna explore the pokedex?" 
+        puts "Type 'yes' to continue or any other key to exit"
+        user_input = gets.strip.downcase
+        if user_input == "yes" || user_input == "y"
+            puts "The user's tale of grand adventure is about to unfold"
+        else 
+            #end the program
+            puts "Ok...You have the ability to become a great Pokemon Professor someday!"
+        end 
+
+    end 
 
 end 
