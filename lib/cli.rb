a4c3b673ca3d02a2b5fa84a56bb73b697e4a7379
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-11-12 11:09:08 -0500

    project

--- lib/cli.rb
+++ lib/cli.rb
@@ -11,11 +11,12 @@ class Cli
     end 
 
     def menu
-        puts "Wannna explore the pokedex?" 
+        puts "Wannna explore the pokedex?"
+        puts "*---------------------*"
         puts "Type 'yes' to continue or any other key to exit"
         user_input = gets.strip.downcase
         if user_input == "yes" || user_input == "y"
-            puts "The user's tale of grand adventure is about to unfold"
+            puts "The user's tale of grand adventure is about to unfold!"
         else 
             #end the program
             puts "Ok...You have the ability to become a great Pokemon Professor someday!"
