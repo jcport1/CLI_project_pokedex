4e3f85072dbdbcd783708750711eccbbc3609b8a
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-11-13 16:01:03 -0500

    project

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,7 +2,7 @@
 class Cli
 
     def start
-
+        
         puts "\n*-------------------------------------*"
         puts "\nWelcome to Pallet Town, Ash Ketchum!"
         puts "\n*-------------------------------------*"
@@ -16,39 +16,30 @@ class Cli
     def menu
 
         sleep(2)
-
         puts "\nI hear you came to learn more about pokemon. Would you like to explore the pokedex?"
-
         sleep(2)
-    
         puts "\nType 'yes' to continue or any other key to exit"
-
-        user_input = gets.strip.downcase
-
+        @user_input = gets.strip.downcase
         if user_input == "yes" || user_input == "y"
+    end 
 
-            puts "\nI'm delighted to hear that! Your grand adventure is about to unfold!"
+    def orig_input(input)
+        
+        puts "\nI'm delighted to hear that! Your grand adventure is about to unfold!"
+        puts "\n"
+        display_list_of_pokemons #call method 
+        ask_user_for_pokemon_choice #call method 
+        sleep(1)
+        menu #call menu to return to menu options 
+        #also possible to have while loop instead 
 
-            puts "\n"
-
-            display_list_of_pokemons #call method 
-
-            ask_user_for_pokemon_choice #call method 
-
-            sleep(1)
-            menu #call menu to return to menu options 
-
-            #Also possible to have while loop instead 
-
-        else 
-            #end the program
-
-            sleep(2)
-            
-            puts "\nOk...You have the ability to become a great Pokemon Professor someday!"
-            puts "\n"
-           
-        end 
+    else 
+        #end the program
+        sleep(2)
+        puts "\nOk...You have the ability to become a great Pokemon Professor someday!"
+        puts "\n"
+       
+    end 
 
     end 
 
@@ -64,11 +55,7 @@ class Cli
 
     def ask_user_for_pokemon_choice
 
-       
         puts "\nPokemon around the world wait for you! Enter a number for more info."
-
-
-
         pokemon = gets.strip.to_i - 1 #ask for user input
         max_limit = Pokemon.all.length - 1
 
@@ -78,7 +65,6 @@ class Cli
         end 
 
         pokemon_instance = Pokemon.all[index]
-
         #instead call the method that will print out the details
         #puts pokemon_instance.name 
         puts "Great choice! Here ya go:"
@@ -86,23 +72,17 @@ class Cli
         sleep (2)
         puts "\n"
         display_pokemon_details(pokemon_instance)
-        
-
     end 
 
     def display_pokemon_details(pokemon)
 
-        API.grab_pokemon_info 
-
+        API.grab_pokemon_info
         #display details
-       puts "\nName: #{pokemon.name}"
-       sleep(1)
-       puts "Height: #{pokemon.height}"
-       puts "Weight: #{pokemon.weight}"
-       puts "Abilities: #{pokemon.abilities}"
-
+        puts "\nName: #{pokemon.name}"
+        sleep(1)
+        puts "Height: #{pokemon.height}"
+        puts "Weight: #{pokemon.weight}"
+        puts "Abilities: #{pokemon.abilities}"
        # puts pokemon.pokemon_data
-
-   end 
-
+     end 
 end 
